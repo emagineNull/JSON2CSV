836edb4f56d96621a37712bfc1c744a1c8e04aa5
Alek Rifai <2033603@brunel.ac.uk>
2023-08-02 20:29:35 +0100

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertTrue;
+
+//import static org.junit.Assert.assertTrue;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> in = new ArrayList<>();
+
+        in = JsonInput.readAndStore(in, scanner);
+
+        String listString = String.join(", ", in);
+        assertTrue(JsonInput.isValid(listString));
+        String newStr = "";
+        newStr = listString.replace("{","");
+        newStr = newStr.replaceAll("}","");
+        System.out.println(newStr);
+
+    }
+}
