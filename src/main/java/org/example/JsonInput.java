60f206f120b9719ee4914225007f56e8f929cfed
Alek Rifai <2033603@brunel.ac.uk>
2023-08-02 20:29:35 +0100

    First commit

---
+++ src/main/java/org/example/JsonInput.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JsonInput {
+    public static ArrayList<String> readAndStore(ArrayList<String> in, Scanner scanner) {
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            in.add(line);
+
+            if (line != null && line.equalsIgnoreCase("END")) {
+                in.remove(in.size() - 1);
+                System.out.println("Output list : " + in);
+                break;
+            }
+        }
+
+        return in;
+    }
+
+    public static boolean isValid(String json) {
+        try {
+            new JSONObject(json);
+        } catch (JSONException e) {
+            try {
+                new JSONArray(json);
+            } catch (JSONException ne) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
